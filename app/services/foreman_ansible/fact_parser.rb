b4405bfc6619f9eaae4eca702d034c6918d0cdb8
Daniel Lobato <elobatocs@gmail.com>
2015-10-30 10:17:39 +0100

    Support interfaces and OS in the fact parser

--- app/services/foreman_ansible/fact_parser.rb
+++ app/services/foreman_ansible/fact_parser.rb
@@ -2,8 +2,14 @@ module ForemanAnsible
   class FactParser < ::FactParser
     attr_reader :facts
 
+    def initialize(facts)
+      @facts = HashWithIndifferentAccess.new(facts[:ansible_facts])
+    end
+
     def operatingsystem
-      facts[:ansible_lsb]
+      args = { :name => os_name, :major => os_major, :minor => os_minor }
+      Operatingsystem.where(args).first ||
+        Operatingsystem.create!(args.merge(:description => os_description))
     end
 
     def environment
@@ -13,10 +19,6 @@ module ForemanAnsible
     def architecture
       name = facts[:ansible_architecture] || facts[:facter_architecture]
       Architecture.where(:name => name).first_or_create unless name.blank?
-    end
-
-    def interfaces
-      facts[:ansible_interfaces]
     end
 
     def model
@@ -36,6 +38,52 @@ module ForemanAnsible
 
     def support_interfaces_parsing?
       true
+    end
+
+    def get_interfaces
+      facts[:ansible_interfaces]
+    end
+
+    def get_facts_for_interface(interface)
+      interface.gsub!(/-/, '_') # virbr1-nic -> virbr1_nic
+      interface_facts = facts[:"ansible_#{interface}"]
+      ipaddress = ip_from_interface(interface)
+      HashWithIndifferentAccess[interface_facts.merge(:ipaddress => ipaddress)]
+    end
+
+    def ipmi_interface
+      # ?
+    end
+
+    private
+
+    def ip_from_interface(interface)
+      return unless facts[:"ansible_#{interface}"]['ipv4'].present?
+      facts[:"ansible_#{interface}"]['ipv4']['address']
+    end
+
+    def os_name
+      facts[:ansible_distribution] ||
+        facts[:ansible_lsb]['id']
+    end
+
+    def os_major
+      facts[:ansible_distribution_major_version] ||
+        facts[:ansible_lsb]['major_release']
+    end
+
+    def os_release
+      facts[:ansible_distribution_version] ||
+        facts[:ansible_lsb]['release']
+    end
+
+    def os_minor
+      _, minor = os_release.split('.')
+      minor
+    end
+
+    def os_description
+      facts[:ansible_lsb]['description']
     end
   end
 end
