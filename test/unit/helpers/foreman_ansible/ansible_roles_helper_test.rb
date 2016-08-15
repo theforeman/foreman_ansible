83a9d763a1b5aba78e46512dead1cfdb816972ba
Tomas Strachota <tstrachota@redhat.com>
2016-08-15 11:07:49 +0200

    Fixes #15837 - update 1
    - UI uses multiple_selects in all cases now
    - Removed available_ansible_roles helper since it's no longer needed

--- test/unit/helpers/foreman_ansible/ansible_roles_helper_test.rb
+++ test/unit/helpers/foreman_ansible/ansible_roles_helper_test.rb
@@ -22,38 +22,6 @@ module ForemanAnsible
           ansible_roles.must_equal @roles
         end
       end
-
-      describe '#available_ansible_roles' do
-        before do
-          @hostgroup_parent = FactoryGirl.create(:hostgroup)
-          @hostgroup = FactoryGirl.create(:hostgroup, :parent => @hostgroup_parent)
-          @host = FactoryGirl.build_stubbed(:host, :hostgroup => @hostgroup)
-          ForemanAnsible::RolesImporter.stubs(:import!)
-        end
-
-        test 'it imports roles' do
-          ForemanAnsible::RolesImporter.expects(:import!)
-          available_ansible_roles(@host)
-        end
-
-        test 'returns all roles for a host without any roles in a hostgroup' do
-          available_ansible_roles(@host).must_equal @roles
-        end
-
-        test 'returns roles complementary to inherited ones for a host' do
-          @hostgroup.ansible_roles << @roles.first
-          available_ansible_roles(@host).must_equal [@role2]
-        end
-
-        test 'returns all roles for a hostgroup without any roles in a parent hostgroup' do
-          available_ansible_roles(@hostgroup).must_equal @roles
-        end
-
-        test 'returns roles complementary to inherited ones for a hostgroup' do
-          @hostgroup_parent.ansible_roles << @roles.first
-          available_ansible_roles(@hostgroup).must_equal [@role2]
-        end
-      end
     end
   end
 end
