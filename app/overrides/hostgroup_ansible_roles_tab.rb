366503af1d3074a44c8fcee8547468daec6da043
Tomas Strachota <tstrachota@redhat.com>
2016-08-16 15:47:42 +0200

    Fixes #15837 - update 3
    - ansible roles tabs now share common partials

--- app/overrides/hostgroup_ansible_roles_tab.rb
+++ app/overrides/hostgroup_ansible_roles_tab.rb
@@ -3,12 +3,12 @@ Deface::Override.new(
   :virtual_path => 'hostgroups/_form',
   :name => 'hostgroup_ansible_roles_tab',
   :insert_after => 'li.active',
-  :partial => 'foreman_ansible/hostgroups/tab_title'
+  :partial => 'foreman_ansible/ansible_roles/select_tab_title'
 )
 
 Deface::Override.new(
   :virtual_path => 'hostgroups/_form',
   :name => 'hostgroup_ansible_roles_tab_content',
   :insert_after => 'div.tab-pane.active',
-  :partial => 'foreman_ansible/hostgroups/tab_content'
+  :partial => 'foreman_ansible/ansible_roles/select_tab_content'
 )
