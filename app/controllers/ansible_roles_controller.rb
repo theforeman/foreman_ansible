180cd25504fe2d19c86f4f4f3f90f7417543598a
Daniel Lobato García <me@daniellobato.me>
2016-09-06 14:58:04 +0200

    Fixes #16442 - Importing from proxy cannot show 'no changes' (#45)
    It tries to use a proxy variable that's not defined, it should use
    '@proxy'

--- app/controllers/ansible_roles_controller.rb
+++ app/controllers/ansible_roles_controller.rb
@@ -24,12 +24,7 @@ class AnsibleRolesController < ::ApplicationController
   def import
     changed = @importer.import!
     if changed.values.all?(&:empty?)
-      if @proxy
-        msg = _('No changes in roles detected on %s.') % proxy.name
-      else
-        msg = _('No changes in roles detected.')
-      end
-      notice msg
+      notice no_changed_roles_message
       redirect_to ansible_roles_path
     else
       render :locals => { :changed => changed }
@@ -51,5 +46,10 @@ class AnsibleRolesController < ::ApplicationController
 
   def create_importer
     @importer = ForemanAnsible::UiRolesImporter.new(@proxy)
+  end
+
+  def no_changed_roles_message
+    return _('No changes in roles detected.') unless @proxy.present?
+    _('No changes in roles detected on %s.') % @proxy.name
   end
 end
