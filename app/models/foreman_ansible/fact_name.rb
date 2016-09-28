8424ca37bb3d92296a2db3192a16af3856ceeb14
Daniel Lobato García <me@daniellobato.me>
2016-09-28 08:04:29 +0200

    Fixes #16656 - Set origin from fact name
    This commit adds the Ansible icon so that it can be shown as the origin
    of some facts, plus it adds the ability of showing it (requires #16643)

--- app/models/foreman_ansible/fact_name.rb
+++ app/models/foreman_ansible/fact_name.rb
@@ -1,3 +1,10 @@
 module ForemanAnsible
-  class FactName < ::FactName; end
+  # Define the class that fact names that come from Ansible should have
+  # It allows us to filter facts by origin, and also to display the origin
+  # in the fact values table (/fact_values)
+  class FactName < ::FactName
+    def origin
+      'Ansible'
+    end
+  end
 end
