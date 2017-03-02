25e4e2b897af728860f3c825d6d8775b58fb68db
Gerrit Schwerthelm <gerrit.schwerthelm@avid.com>
2017-03-02 10:41:40 +0100

    Fixes #18104 - Ad hoc Ansible role execution
    Especially when using Foreman as a backend service, there is yet no
    possibility to assign roles to hosts over the API. In addition to that,
    some scenarios (like maintenance tasks) require only one-time-execution
    of a role or rare role execution from time to time. The need to change
    role assignments for each host to do these kind of tasks is impractical.
    Therefore, an API where there is no need of specific role assignments
    and where an imported role can be executed directly against a certain
    host helps to broaden the scopes of application of the Foreman Ansible
    plugin. Hopefully, the community is also interested in such
    functionality.

--- app/assets/javascripts/foreman_ansible/show_ad_hoc_role_modal.js
+++ app/assets/javascripts/foreman_ansible/show_ad_hoc_role_modal.js
@@ -1,11 +1,11 @@
-function show_ad_hoc_role_modal(modal_id = null) {
-  var modal_window = modal_id === null ? $('#adHocRoleModal') : $('#adHocRoleModal' + modal_id);
+function show_ad_hoc_role_modal(modal_id) {
+  var modal_window = !modal_id ? $('#adHocRoleModal') : $('#adHocRoleModal' + modal_id);
   modal_window.modal({'show': true});
   modal_window.find('a[rel="popover-modal"]').popover();
   activate_select2(modal_window);
 }
 
-function close_ad_hoc_role_modal(modal_id = null) {
-  var modal_window = modal_id === null ? $('#adHocRoleModal') : $('#adHocRoleModal' + modal_id);
+function close_ad_hoc_role_modal(modal_id) {
+  var modal_window = !modal_id ? $('#adHocRoleModal') : $('#adHocRoleModal' + modal_id);
   modal_window.modal('hide');
 }
