fa526b834617709fe2e19fe630716911ea5595ec
Sebastian Gräßl <bastilian@users.noreply.github.com>
2017-10-06 11:05:14 +0200

    Fixes #21005 - Better wording for roles import button

--- app/helpers/foreman_ansible/ansible_roles_helper.rb
+++ app/helpers/foreman_ansible/ansible_roles_helper.rb
@@ -3,17 +3,24 @@ module ForemanAnsible
   module AnsibleRolesHelper
     def ansible_proxy_links(hash, classes = nil)
       links = SmartProxy.with_features('Ansible').map do |proxy|
-        display_link_if_authorized(_('Import from %s') % proxy.name,
+        display_link_if_authorized(_('From %s') % proxy.name,
                                    hash.merge(:proxy => proxy),
                                    :class => classes)
       end.flatten
-      links.unshift display_link_if_authorized(_('Import from Foreman host'),
+      host_text = if links.any?
+                    _('From Foreman host')
+                  else
+                    _('Import from Foreman host')
+                  end
+      links.unshift display_link_if_authorized(host_text,
                                                hash,
                                                :class => classes)
     end
 
     def ansible_proxy_import(hash)
-      select_action_button(_('Import'), {}, ansible_proxy_links(hash))
+      select_action_button(_('Import'),
+                           { :primary => true, :class => 'roles-import' },
+                           ansible_proxy_links(hash))
     end
 
     def import_time(role)
