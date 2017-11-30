b875370110c92d9b6f2d676d46391dbd70b29717
Ondrej Prazak <oprazak@redhat.com>
2017-11-30 13:54:10 +0100

    Fixes #21818 - Add Rails 5 style migrations

--- db/migrate/20160707195442_create_host_ansible_roles.rb
+++ db/migrate/20160707195442_create_host_ansible_roles.rb
@@ -1,5 +1,5 @@
 # Renames join table between Host and Ansible Roles
-class CreateHostAnsibleRoles < ActiveRecord::Migration
+class CreateHostAnsibleRoles < ActiveRecord::Migration[4.2]
   def change
     rename_table :ansible_roles_hosts, :host_ansible_roles
     add_column :host_ansible_roles, :id, :primary_key
