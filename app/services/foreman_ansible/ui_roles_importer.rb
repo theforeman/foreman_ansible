c92d821d6b4a873c2331671b13536ed03f07f08f
Daniel Lobato Garcia <me@daniellobato.me>
2018-01-10 03:06:51 +0000

    Update fixtures and fix rubocop

--- app/services/foreman_ansible/ui_roles_importer.rb
+++ app/services/foreman_ansible/ui_roles_importer.rb
@@ -11,16 +11,18 @@ module ForemanAnsible
       delete_old_roles changes['obsolete'] if changes['obsolete']
     end
 
+    # rubocop:disable Performance/HashEachMethods
     def create_new_roles(changes)
-      changes.each_value do |new_role|
+      changes.values.each do |new_role|
         ::AnsibleRole.create(JSON.parse(new_role))
       end
     end
 
     def delete_old_roles(changes)
-      changes.each_value do |old_role|
+      changes.values.each do |old_role|
         ::AnsibleRole.find(JSON.parse(old_role)['id']).destroy
       end
     end
+    # rubocop:enable Performance/HashEachMethods
   end
 end
