19d7aba057d0ce1be2a0d3a1679cf06e7d1034fe
Daniel Lobato Garcia <me@daniellobato.me>
2018-02-01 17:04:00 +0100

    Release 2.0.2 foreman_ansible_core

--- lib/foreman_ansible_core.rb
+++ lib/foreman_ansible_core.rb
@@ -1,4 +1,5 @@
 require 'foreman_tasks_core'
+require 'foreman_remote_execution_core'
 
 # Core actions for Foreman Ansible, used by both Foreman and Foreman proxy
 # This comprises running playbooks for the moment
@@ -14,15 +15,13 @@ module ForemanAnsibleCore
     require 'foreman_ansible_core/actions'
   end
 
-  if defined? ForemanRemoteExecutionCore
-    require 'foreman_remote_execution_core/actions'
-    require 'foreman_ansible_core/remote_execution_core/ansible_runner'
-    require 'foreman_ansible_core/remote_execution_core/settings_override'
-    ForemanRemoteExecutionCore::Actions::RunScript.send(
-      :prepend,
-      ForemanAnsibleCore::RemoteExecutionCore::SettingsOverride
-    )
-  end
+  require 'foreman_remote_execution_core/actions'
+  require 'foreman_ansible_core/remote_execution_core/ansible_runner'
+  require 'foreman_ansible_core/remote_execution_core/settings_override'
+  ForemanRemoteExecutionCore::Actions::RunScript.send(
+    :prepend,
+    ForemanAnsibleCore::RemoteExecutionCore::SettingsOverride
+  )
 
   require 'foreman_ansible_core/roles_reader'
   require 'foreman_ansible_core/version'
