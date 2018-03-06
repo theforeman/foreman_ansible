df5f17d8c78a53a6081cb701d9a595ae380d695a
Daniel Lobato Garcia <me@daniellobato.me>
2018-03-06 10:37:10 +0000

    Refs #22784 - Require VERSION to show documentation

--- app/helpers/foreman_ansible/ansible_plugin_helper.rb
+++ app/helpers/foreman_ansible/ansible_plugin_helper.rb
@@ -1,8 +1,12 @@
+require "#{ForemanAnsible::Engine.root}/lib/foreman_ansible/version"
+
 module ForemanAnsible
   # General helper for foreman_ansible
   module AnsiblePluginHelper
     def ansible_doc_url
-      'http://theforeman.org/plugins/foreman_ansible/1.x/index.html'
+      major_version = ::ForemanAnsible::VERSION.split('.')[0]
+      'https://theforeman.org/plugins/foreman_ansible/'\
+        "#{major_version}.x/index.html"
     end
   end
 end
