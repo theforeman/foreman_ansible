2bc1ef112afa39184b6ec2ce51225c70e33c2353
Sebastian Gräßl <mail@bastilian.me>
2018-03-14 15:14:46 +0100

    Fixes #22896 - Fall back to REX private key

--- app/services/foreman_ansible/inventory_creator.rb
+++ app/services/foreman_ansible/inventory_creator.rb
@@ -87,6 +87,7 @@ module ForemanAnsible
         'ansible_become' => @template_invocation.effective_user,
         'ansible_user' => host_setting(host, 'remote_execution_ssh_user'),
         'ansible_ssh_pass' => rex_ssh_password(host),
+        'ansible_ssh_private_key_file' => ansible_or_rex_ssh_private_key(host),
         'ansible_port' => host_setting(host, 'remote_execution_ssh_port')
       }
       # Backward compatibility for Ansible 1.x
@@ -106,6 +107,15 @@ module ForemanAnsible
     def rex_ssh_password(host)
       @template_invocation.job_invocation.password ||
         host_setting(host, 'remote_execution_ssh_password')
+    end
+
+    def ansible_or_rex_ssh_private_key(host)
+      ansible_private_file = host_setting(host, 'ansible_ssh_private_key_file')
+      if !ansible_private_file.empty?
+        ansible_private_file
+      else
+        ForemanRemoteExecutionCore.settings[:ssh_identity_key_file]
+      end
     end
 
     private
