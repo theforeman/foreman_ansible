cb660ad11c15a7d1630ddd14eb65aeb39c32b84a
Marek Hulan <mhulan@redhat.com>
2018-03-21 23:39:00 +0100

    Fixes #22973 - add custom insights notification

---
+++ db/seeds.d/90_notification_blueprints.rb
@@ -0,0 +1,17 @@
+blueprints = [
+  {
+    :group => N_('Jobs'),
+    :name => 'insights_remediation_successful',
+    :message => N_('Insights remediation on %{hosts_count}' \
+                   ' host(s) has finished successfully'),
+    :level => 'success',
+    :actions =>           {
+      :links => [
+        :path_method => :job_invocation_path,
+        :title => N_('Job Details')
+      ]
+    }
+  }
+]
+
+blueprints.each { |blueprint| UINotifications::Seed.new(blueprint).configure }
