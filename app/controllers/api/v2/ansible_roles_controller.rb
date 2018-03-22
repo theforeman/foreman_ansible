ffbe9537aa0608e36d2e316a4bc78d7c796f6b29
Oleh Fedorenko <ofedoren@redhat.com>
2018-03-22 15:39:20 +0000

    Fixes #22994 - Add refactor for ansible roles controller

--- app/controllers/api/v2/ansible_roles_controller.rb
+++ app/controllers/api/v2/ansible_roles_controller.rb
@@ -4,33 +4,38 @@ module Api
     class AnsibleRolesController < ::Api::V2::BaseController
       include ::Api::Version2
 
+      resource_description do
+        api_version 'v2'
+        api_base_url '/ansible/api'
+      end
+
       before_action :find_resource, :only => [:show, :destroy]
       before_action :find_proxy, :only => [:import, :obsolete]
       before_action :create_importer, :only => [:import, :obsolete]
 
-      api :GET, '/ansible/ansible_roles/:id', N_('Show role')
+      api :GET, '/ansible_roles/:id', N_('Show role')
       param :id, :identifier, :required => true
       def show; end
 
-      api :GET, '/ansible/ansible_roles', N_('List Ansible roles')
+      api :GET, '/ansible_roles', N_('List Ansible roles')
       param_group :search_and_pagination, ::Api::V2::BaseController
       def index
         @ansible_roles = resource_scope_for_index
       end
 
-      api :DELETE, '/ansible/ansible_roles/:id', N_('Deletes Ansible role')
+      api :DELETE, '/ansible_roles/:id', N_('Deletes Ansible role')
       param :id, :identifier, :required => true
       def destroy
         process_response @ansible_role.destroy
       end
 
-      api :POST, '/ansible/ansible_roles/import', N_('Import Ansible roles')
+      api :POST, '/ansible_roles/import', N_('Import Ansible roles')
       param :proxy, Hash, N_('Smart Proxy to import from')
       def import
         @imported = @importer.import!
       end
 
-      api :POST, '/ansible/ansible_roles/obsolete', N_('Obsolete Ansible roles')
+      api :POST, '/ansible_roles/obsolete', N_('Obsolete Ansible roles')
       param :proxy, Hash, N_('Smart Proxy to import from')
       def obsolete
         @obsoleted = @importer.obsolete!
