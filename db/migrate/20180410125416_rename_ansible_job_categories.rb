88d15d1c70544d9752ece19aa76f6dffb6a3078e
Marek Hulan <mhulan@redhat.com>
2018-04-17 03:59:56 -0400

    Refs #23205 - fix the migration on existing setups

--- db/migrate/20180410125416_rename_ansible_job_categories.rb
+++ db/migrate/20180410125416_rename_ansible_job_categories.rb
@@ -7,21 +7,23 @@ class RenameAnsibleJobCategories < ActiveRecord::Migration[5.1]
     User.as_anonymous_admin do
       updated_templates = ['Power Action - Ansible Default',
                            'Puppet Run Once - Ansible Default']
-      job_templates = JobTemplate.without_auditing.where(
-        :name => updated_templates
-      ).all
-      job_templates.each do |job_template|
-        next if job_template.job_category =~ /^Ansible/
-        job_template.job_category = "Ansible #{job_template.job_category}"
-        job_template.save_without_auditing
-      end
-
-      service_template = JobTemplate.where(
-        :name => 'Service Action - Ansible Default'
-      ).first
-      if service_template.present?
-        service_template.job_category = 'Ansible Services'
-        service_template.save_without_auditing
+      JobTemplate.without_auditing do
+        job_templates = JobTemplate.where(
+          :name => updated_templates
+        ).all
+        job_templates.each do |job_template|
+          next if job_template.job_category =~ /^Ansible/
+          job_template.job_category = "Ansible #{job_template.job_category}"
+          job_template.save_without_auditing
+        end
+  
+        service_template = JobTemplate.where(
+          :name => 'Service Action - Ansible Default'
+        ).first
+        if service_template.present?
+          service_template.job_category = 'Ansible Services'
+          service_template.save_without_auditing
+        end
       end
     end
   end
