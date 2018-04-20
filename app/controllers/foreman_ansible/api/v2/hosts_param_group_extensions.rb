789350920ba5d19c33bffae17516a4961d9cf418
Oleh Fedorenko <ofedoren@redhat.com>
2018-04-20 12:52:20 +0000

    Fixes #23229 - Extend the host(group)'s params

---
+++ app/controllers/foreman_ansible/api/v2/hosts_param_group_extensions.rb
@@ -0,0 +1,17 @@
+module ForemanAnsible
+  module Api
+    module V2
+      # Extends the hosts api controller to support creating/updating with roles
+      module HostsParamGroupExtensions
+        extend Apipie::DSL::Concern
+
+        update_api(:create, :update) do
+          param :host, Hash do
+            param :ansible_role_ids, Array,
+                  :desc => N_('IDs of associated ansible roles')
+          end
+        end
+      end
+    end
+  end
+end
--- test/functional/api/v2/hosts_controller_test.rb
+++ test/functional/api/v2/hosts_controller_test.rb
@@ -5,6 +5,8 @@ module Api
     # Tests for the extra methods to play roles on a Host
     class HostsControllerTest < ActionController::TestCase
       setup do
+        @ansible_role1 = FactoryBot.create(:ansible_role)
+        @ansible_role2 = FactoryBot.create(:ansible_role)
         @host1 = FactoryBot.create(:host)
         @host2 = FactoryBot.create(:host)
       end
@@ -35,6 +37,30 @@ module Api
         post :multiple_play_roles, :params => { :id => targets }
         response = JSON.parse(@response.body)
         assert_job_invocation_is_ok(response, targets)
+      end
+
+      test 'should create a host with ansible_role_ids param' do
+        post :create,
+             :params => { :host => { :ansible_role_ids => @ansible_role1.id,
+                                     :managed => false,
+                                     :name => 'Doe' } },
+             :session => set_session_user
+        assert_response :created
+        assert assigns('host').ansible_roles, [@ansible_role1]
+      end
+
+      test 'should update a host with ansible_role_ids param' do
+        host = FactoryBot.create(:host,
+                                 :managed => false,
+                                 :ansible_role_ids => [@ansible_role1.id])
+        post :update,
+             :params => {
+               :id => host.id,
+               :host => { :ansible_role_ids => [@ansible_role2.id] }
+             },
+             :session => set_session_user
+        assert_response :success
+        assert assigns('host').ansible_roles, [@ansible_role2]
       end
     end
   end
