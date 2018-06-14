596e5ef4df853198e7e9881d262f4e27e157e914
Daniel Lobato Garcia <me@daniellobato.me>
2018-06-14 08:51:06 +0000

    Fixes #23545 - Move post-provisioning Ansible to REX
    Leftovers from when foreman_ansible was not a REX provider. It now uses
    the :ansible_run_host REX feature to run the playbook with all roles.
    
    Use timeout setting

--- app/models/concerns/foreman_ansible/host_managed_extensions.rb
+++ app/models/concerns/foreman_ansible/host_managed_extensions.rb
@@ -2,7 +2,9 @@ module ForemanAnsible
   # Relations to make Host::Managed 'have' ansible roles
   module HostManagedExtensions
     extend ActiveSupport::Concern
+    include ::ForemanAnsible::Concerns::JobInvocationHelper
 
+    # rubocop:disable Metrics/BlockLength
     included do
       has_many :host_ansible_roles, :foreign_key => :host_id
       has_many :ansible_roles, :through => :host_ansible_roles,
@@ -20,18 +22,28 @@ module ForemanAnsible
         hostgroup.all_ansible_roles
       end
 
+      # This one should be fixed, disabled for the moment as we're
+      # in a rush to get the release out
+      # rubocop:disable Metrics/AbcSize
       def play_ansible_roles
-        return unless ansible_roles.present? || inherited_ansible_roles.present?
-        task = ::ForemanTasks.async_task(
-          ::Actions::ForemanAnsible::PlayHostRoles,
-          self,
-          ::ForemanAnsible::ProxySelector.new,
-          :timeout => Setting['ansible_post_provision_timeout']
+        return true unless ansible_roles.present? ||
+                           inherited_ansible_roles.present?
+        composer = job_composer(:ansible_run_host, self)
+        composer.triggering.mode = :future
+        composer.triggering.start_at = (
+          Time.zone.now +
+          Setting::Ansible[:ansible_post_provision_timeout].to_i.seconds
         )
+        composer.trigger!
         logger.info("Task for Ansible roles on #{self} before_provision: "\
-                    "#{Rails.application.routes.url_helpers.task_path(task)}.")
+                    "#{job_invocation_path(composer.job_invocation)}")
+      rescue Foreman::Exception => e
+        logger.info("Error running Ansible roles on #{self} before_provision: "\
+                    "#{e.message}")
       end
+      # rubocop:enable Metrics/AbcSize
     end
+    # rubocop:enable Metrics/BlockLength
   end
 end
 
