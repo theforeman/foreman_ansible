fa01cfeac79c2b42bee7bd977ba6584a4f53ad93
Lukas Zapletal <lzap+git@redhat.com>
2018-09-25 14:18:20 +0200

    Fixes #25030 - importer works with Parameters

--- app/services/foreman_ansible/ui_roles_importer.rb
+++ app/services/foreman_ansible/ui_roles_importer.rb
@@ -14,13 +14,13 @@ module ForemanAnsible
     end
 
     def create_new_roles(changes)
-      changes.each_value do |new_role|
+      changes.each_pair do |_, new_role|
         ::AnsibleRole.create(JSON.parse(new_role))
       end
     end
 
     def delete_old_roles(changes)
-      changes.each_value do |old_role|
+      changes.each_pair do |_, old_role|
         ::AnsibleRole.find(JSON.parse(old_role)['id']).destroy
       end
     end
