a7a4335a1383c13c16c62ab047598518ffd0f2dc
Stephen Benjamin <stephen@bitbin.de>
2018-10-26 09:32:27 -0400

    fixes #25009 - fix sync script and sync templates (#209)

--- script/sync_templates.sh
+++ script/sync_templates.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 #
-# Copies unattended templates from community-templates repository to
-# app/views/unattended/ where they can be seeded on new installations.
+# Copies job templates from community-templates repository to
+# app/views/foreman_ansbile/job_templates so they can be seeded on new
+# installations.
 #
-# Not intended for use on existing installations, only for development.
-# Production installations should use the foreman_templates plugin to
-# update the contents of the database.
+# Part of the release process.
 
 REPO=$(mktemp -d)
 trap "rm -rf $REPO" EXIT
+shopt -s extglob
 
-git clone -q -b develop \
-  https://github.com/theforeman/community-templates $REPO/ct
+git clone -q -b develop https://github.com/theforeman/community-templates $REPO/ct
 
-# move into destination dir if run from Foreman root
-[ -d app/views/foreman_ansible/ ] && cd app/views/foreman_ansible/
+pushd $PWD
+cd $REPO/ct/job_templates
 
-rsync -am \
-  --include='*ansible*' \
-  --exclude='*' \
-  $REPO/ct/job_templates/* ./job_templates
+# Delete all non-ansible templates and remove suffix
+rm !(*_-_ansible_default.erb)
+
+popd
+
+# Move into destination dir if run from plugin root
+[ -d app/views/templates/ssh ] && cd app/views/templates/ssh
+[ -d app/views/foreman_ansible/job_templates/ ] && cd app/views/foreman_ansible/job_templates/
+
+rsync -r \
+  --exclude README.md \
+  --exclude '.*' \
+  --exclude test \
+  --exclude Rakefile \
+  $REPO/ct/job_templates/*.erb ./
 
 cd -
 
 git status -- app/views/foreman_ansible/job_templates
+
+if [ $(git status --porcelain -u -- app/views/foreman_ansible/job_templates | grep -c '^\?') -gt 0 ]; then
+  echo
+  echo "Warning: new files copied, be sure to commit them and ensure they are seeded."
+fi
