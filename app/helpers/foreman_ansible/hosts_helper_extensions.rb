c8f0bf8c1ec0d4ee61ebdb3b5ab65b8e26b6ef33
Marek Hulán <ares@users.noreply.github.com>
2019-02-13 14:18:13 +0100

    Fixes #25989 - move play roles button (#242)

--- app/helpers/foreman_ansible/hosts_helper_extensions.rb
+++ app/helpers/foreman_ansible/hosts_helper_extensions.rb
@@ -6,16 +6,6 @@ module ForemanAnsible
     extend ActiveSupport::Concern
 
     module Overrides
-      def host_title_actions(*args)
-        host = args.first
-        if ansible_roles_present?(host) &&
-           User.current.can?(:create_job_invocations)
-          button = ansible_roles_button(host)
-          title_actions(button_group(button))
-        end
-        super(*args)
-      end
-
       def multiple_actions
         actions = super
         if User.current.can?(:create_job_invocations) &&
@@ -35,16 +25,6 @@ module ForemanAnsible
     def ansible_roles_present?(host)
       host.ansible_roles.present? ||
         host.inherited_ansible_roles.present?
-    end
-
-    def ansible_roles_button(host)
-      display_link_if_authorized(
-        _('Run Ansible roles'),
-        hash_for_play_roles_host_path(:id => host.id),
-        :id => :ansible_roles_button,
-        :class => 'btn btn-default',
-        :'data-no-turbolink' => true
-      )
     end
   end
 end
