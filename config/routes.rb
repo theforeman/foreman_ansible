394edbcb29674b4373ae9d1ccbbbc4ed699487cc
Tomer Brisker <tbrisker@gmail.com>
2019-02-18 10:40:10 +0200

    Fix rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@
 Rails.application.routes.draw do
   namespace :api do
     scope '(:apiv)',
-          :module      => :v2,
-          :defaults    => { :apiv => 'v2' },
-          :apiv        => /v1|v2/,
+          :module => :v2,
+          :defaults => { :apiv => 'v2' },
+          :apiv => /v1|v2/,
           :constraints => ApiConstraints.new(:version => 2) do
 
       constraints(:id => %r{[^\/]+}) do
@@ -67,9 +67,9 @@ Rails.application.routes.draw do
 
     namespace :api do
       scope '(:apiv)',
-            :module      => :v2,
-            :defaults    => { :apiv => 'v2' },
-            :apiv        => /v1|v2/,
+            :module => :v2,
+            :defaults => { :apiv => 'v2' },
+            :apiv => /v1|v2/,
             :constraints => ApiConstraints.new(:version => 2) do
 
         resources :ansible_roles, :only => [:show, :index, :destroy] do
