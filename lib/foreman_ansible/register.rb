8896e8fbe6b84fb99e8b998231a92545ad447f85
Ondřej Pražák <xprazak2@users.noreply.github.com>
2019-02-21 16:07:35 +0100

    Fixes #26101 - Add missing permissions (#247)
    (cherry picked from commit b5ec78f6bfbf333ff7f0de9e2596f80affdde327)

--- lib/foreman_ansible/register.rb
+++ lib/foreman_ansible/register.rb
@@ -14,7 +14,7 @@ Foreman::Plugin.register :foreman_ansible do
                                           :multiple_play_roles] },
                :resource_type => 'Hostgroup'
     permission :view_ansible_roles,
-               { :ansible_roles => [:index],
+               { :ansible_roles => [:index, :auto_complete_search],
                  :'api/v2/ansible_roles' => [:index, :show, :fetch] },
                :resource_type => 'AnsibleRole'
     permission :destroy_ansible_roles,
