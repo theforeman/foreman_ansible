503b71a0cf8c9b6ee1900d5e3353c434483e9fb7
Avram Lubkin <avylove@rockhopper.net>
2019-07-18 12:38:33 -0400

    Fixes #27285 - Only provide parameterize roles in ENC

--- app/services/foreman_ansible/ansible_info.rb
+++ app/services/foreman_ansible/ansible_info.rb
@@ -1,22 +1,7 @@
 module ForemanAnsible
   class AnsibleInfo < ::HostInfo::Provider
     def host_info
-      if Setting[:ansible_parameterized_roles_in_enc]
-        { 'roles' => ansible_roles }
-      else
-        { 'parameters' => ansible_params }
-      end
-    end
-
-    def ansible_params
-      variables = AnsibleVariable.where(:ansible_role_id => host.all_ansible_roles.pluck(:id), :override => true)
-      values = variables.values_hash(host)
-
-      variables.each_with_object({}) do |var, memo|
-        value = values[var]
-        memo[var.key] = value if value
-        memo
-      end
+      { 'ansible_roles' => ansible_roles }
     end
 
     def ansible_roles
@@ -34,3 +19,4 @@ module ForemanAnsible
     end
   end
 end
+
