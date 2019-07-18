5a4ec7ddac37a480c2433c7bd5f6b676042dab7f
Ondrej Prazak <oprazak@redhat.com>
2019-07-18 13:35:14 +0200

    Release 3.0.4

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '3.0.3'
+  VERSION = '3.0.4'
 end
