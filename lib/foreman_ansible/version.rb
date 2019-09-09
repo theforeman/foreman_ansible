2802b7664396fe3cff675d498f107df47f94d8dd
Marek Hulan <mhulan@redhat.com>
2019-09-09 17:13:50 +0200

    Bump Foreman dependency

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '3.0.5'
+  VERSION = '4.0.0'
 end
