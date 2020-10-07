7311dedf3f975c3aff59a97ed9091a4f737b68cc
Nagoor Shaik <nagoor.s@gmail.com>
2020-10-07 13:46:17 +0530

    Fixes #26851 - refactor ansible password params
    (cherry picked from commit 41b5f86bd469df9018f7e043359613b3c3373aaa)

--- lib/foreman_ansible_core/runner/playbook.rb
+++ lib/foreman_ansible_core/runner/playbook.rb
@@ -124,7 +124,7 @@ module ForemanAnsibleCore
           per_host = secrets['per-host'][name]
 
           new_secrets = {
-            'ansible_ssh_pass' => inventory['ssh_password'] || per_host['ansible_ssh_pass'],
+            'ansible_password' => inventory['ssh_password'] || per_host['ansible_password'],
             'ansible_become_password' => inventory['effective_user_password'] || per_host['ansible_become_password']
           }
           inventory['_meta']['hostvars'][name].update(new_secrets)
--- test/unit/lib/foreman_ansible_core/ansible_runner_test.rb
+++ test/unit/lib/foreman_ansible_core/ansible_runner_test.rb
@@ -24,7 +24,7 @@ module ForemanAnsibleCore
             '_meta' => { 'hostvars' => { 'foreman.example.com' => {} } } }
         end
         let(:input) do
-          host_secrets = { 'ansible_ssh_pass' => 'letmein', 'ansible_become_password' => 'iamroot' }
+          host_secrets = { 'ansible_password' => 'letmein', 'ansible_become_password' => 'iamroot' }
           secrets = { 'per-host' => { 'foreman.example.com' => host_secrets } }
           host_input = { 'input' => { 'action_input' => { 'secrets' => secrets } } }
           { 'foreman.example.com' => host_input }
@@ -35,14 +35,14 @@ module ForemanAnsibleCore
           test_inventory = inventory.merge('ssh_password' => 'sshpass', 'effective_user_password' => 'mypass')
           rebuilt = runner.send(:rebuild_secrets, test_inventory, input)
           host_vars = rebuilt.dig('_meta', 'hostvars', 'foreman.example.com')
-          assert_equal 'sshpass', host_vars['ansible_ssh_pass']
+          assert_equal 'sshpass', host_vars['ansible_password']
           assert_equal 'mypass', host_vars['ansible_become_password']
         end
 
         test 'host secrets are used when not overriden by inventory secrest' do
           rebuilt = runner.send(:rebuild_secrets, inventory, input)
           host_vars = rebuilt.dig('_meta', 'hostvars', 'foreman.example.com')
-          assert_equal 'letmein', host_vars['ansible_ssh_pass']
+          assert_equal 'letmein', host_vars['ansible_password']
           assert_equal 'iamroot', host_vars['ansible_become_password']
         end
       end
