133b92086d1e686406e916f6a70b6f6f6fec948a
Ondrej Prazak <oprazak@redhat.com>
2021-03-12 15:49:12 +0100

    Fixes #32080 - Run Ansible with flags for tags
    Adds support for --tags and --skip-tags

--- app/models/foreman_ansible/ansible_provider.rb
+++ app/models/foreman_ansible/ansible_provider.rb
@@ -18,6 +18,10 @@ if defined? ForemanRemoteExecution
           'Ansible'
         end
 
+        def provider_input_namespace
+          :ansible
+        end
+
         def proxy_command_options(template_invocation, host)
           super(template_invocation, host).merge(
             'ansible_inventory' => ::ForemanAnsible::InventoryCreator.new(
@@ -57,6 +61,51 @@ if defined? ForemanRemoteExecution
 
         def supports_effective_user?
           true
+        end
+
+        def provider_inputs
+          [
+            ForemanRemoteExecution::ProviderInput.new(
+              name: 'tags',
+              label: _('Tags'),
+              value: '',
+              value_type: 'plain',
+              description: 'Tags used for Ansible execution'
+            ),
+            ForemanRemoteExecution::ProviderInput.new(
+              name: 'tags_flag',
+              label: _('Include/Exclude Tags'),
+              value: 'include',
+              description: 'Option whether to include or exclude tags',
+              options: "include\nexclude"
+            )
+          ]
+        end
+
+        def provider_inputs_doc
+          opts = provider_inputs.find { |input| input.name == 'tags_flag' }.options.split("\n")
+          {
+            :namespace => provider_input_namespace,
+            :opts => { :desc => N_('Ansible provider specific inputs') },
+            :children => [
+              {
+                :name => :tags,
+                :type => Array,
+                :opts => { :required => false, :desc => N_('A comma separated list of tags to use for Ansible run') }
+              },
+              {
+                :name => :tags_flag,
+                :type => opts,
+                :opts => { :required => false, :desc => N_('Include\Exclude tags for Ansible run') }
+              }
+            ]
+          }
+        end
+
+        def proxy_command_provider_inputs(template_invocation)
+          tags = template_invocation.provider_input_values.find_by(:name => 'tags')&.value || ''
+          tags_flag = template_invocation.provider_input_values.find_by(:name => 'tags_flag')&.value || ''
+          { :tags => tags, :tags_flag => tags_flag }
         end
 
         def proxy_operation_name
