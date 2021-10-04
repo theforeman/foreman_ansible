202fade163113ed6e44728c0e0b0216ca974b9c4
Ondrej Prazak <oprazak@redhat.com>
2021-10-04 13:49:49 +0200

    Fixes #33679 - Add debug for adding positions to roles

--- db/migrate/20210120150019_add_position_to_ansible_role.rb
+++ db/migrate/20210120150019_add_position_to_ansible_role.rb
@@ -10,17 +10,25 @@ class AddPositionToAnsibleRole < ActiveRecord::Migration[6.0]
   end
 
   def update_host_ansible_roles
-    HostAnsibleRole.all.pluck(:host_id, :id).group_by(&:first).each do |_host_id, role_ids|
-      role_ids.each_with_index do |(_host_id, host_role_id), idx|
-        HostAnsibleRole.find(host_role_id).update(position: idx + 1)
+    HostAnsibleRole.all.pluck(:host_id, :id, :ansible_role_id).group_by(&:first).each do |_host_id, role_ids|
+      role_ids.each_with_index do |(host_id, host_role_id, ansible_role_id), idx|
+        har = HostAnsibleRole.find(host_role_id)
+        unless har.update(position: idx + 1) do
+          say "Failed to update order for Ansible role ID #{ansible_role_id} and host ID #{host_id}, association ID #{host_role_id}"
+          say har.errors.full_messages
+        end
       end
     end
   end
 
   def update_hostgroup_ansible_roles
-    HostgroupAnsibleRole.all.pluck(:hostgroup_id, :id).group_by(&:first).each do |_hostgroup_id, role_ids|
-      role_ids.each_with_index do |(_hostgroup_id, hostgroup_role_id), idx|
-        HostgroupAnsibleRole.find(hostgroup_role_id).update(position: idx + 1)
+    HostgroupAnsibleRole.all.pluck(:hostgroup_id, :id, :ansible_role_id).group_by(&:first).each do |_hostgroup_id, role_ids|
+      role_ids.each_with_index do |(hostgroup_id, hostgroup_role_id, ansible_role_id), idx|
+        har = HostgroupAnsibleRole.find(hostgroup_role_id)
+        unless har.update(position: idx + 1) do
+          say "Failed to update order for Ansible role ID #{ansible_role_id} and hostgroup ID #{hostgroup_id}, association ID #{hostgroup_role_id}"
+          say har.errors.full_messages
+        end
       end
     end
   end
