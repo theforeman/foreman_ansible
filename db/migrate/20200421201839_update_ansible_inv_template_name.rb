ff8f08b34df4bb7a0aeed1a2d1a7ae562fd90113
Ondřej Ezr <oezr@redhat.com>
2022-03-11 16:52:56 +0100

    Dont update setting defaults

--- db/migrate/20200421201839_update_ansible_inv_template_name.rb
+++ db/migrate/20200421201839_update_ansible_inv_template_name.rb
@@ -1,9 +1,5 @@
 class UpdateAnsibleInvTemplateName < ActiveRecord::Migration[5.2]
   def up
-    Setting.where(:name => 'ansible_inventory_template').update_all(:default => 'Ansible - Ansible Inventory')
-  end
-  
-  def down
-    Setting.where(:name => 'ansible_inventory_template').update_all(:default => 'Ansible Inventory')
+    # Setting defaults are updated automatically now
   end
 end
