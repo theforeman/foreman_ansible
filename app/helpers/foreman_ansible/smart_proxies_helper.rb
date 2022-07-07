4df6420d05c4bfb8ca76e4465643e7c2d0826627
Adam Ruzicka <a.ruzicka@outlook.com>
2022-07-07 15:34:07 +0200

    Fixes #35143 - Add a smart proxy update button

---
+++ app/helpers/foreman_ansible/smart_proxies_helper.rb
@@ -0,0 +1,39 @@
+module ForemanAnsible
+  module SmartProxiesHelper
+    def can_update_proxy?(proxy)
+      hosts = proxy.smart_proxy_hosts
+
+      return if !can_schedule_jobs? ||
+                hosts.empty? ||
+                !hosts.all? { |host| can_execute_on_host?(host) }
+
+      begin
+        version = proxy.statuses[:version].version
+      rescue Foreman::Exception
+        return false
+      end
+
+      foreman_version = Foreman::Version.new
+      proxy_version = Foreman::Version.new(version['version'])
+
+      foreman_major = foreman_version.major.to_i
+      foreman_minor = foreman_version.minor.to_i
+
+      proxy_major = proxy_version.major.to_i
+      proxy_minor = proxy_version.minor.to_i
+
+      foreman_major > proxy_major ||
+        (foreman_major == proxy_major && foreman_minor > proxy_minor)
+    rescue
+    end
+
+    def proxy_update_button(proxy)
+      feature = RemoteExecutionFeature.feature(:ansible_run_capsule_upgrade)
+      return if feature.nil?
+
+      path = job_invocations_path(:host_ids => proxy.infrastructure_host_facets.pluck(:host_id),
+                                  :feature => feature.label)
+      link_to(_('%s') % feature.name, path, :method => :post)
+    end
+  end
+end
