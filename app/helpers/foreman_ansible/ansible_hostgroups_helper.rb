e6323aa19538412b30e8277d8b57cfceefdc9267
nofaralfasi <nalfassi@redhat.com>
2022-08-15 13:35:02 +0300

    Fixes #28374 - disable run ansible roles from hostgroup dropdown option if no roles are assigned

--- app/helpers/foreman_ansible/ansible_hostgroups_helper.rb
+++ app/helpers/foreman_ansible/ansible_hostgroups_helper.rb
@@ -4,7 +4,7 @@ module ForemanAnsible
   module AnsibleHostgroupsHelper
     def ansible_hostgroups_actions(hostgroup)
       play_roles = if hostgroup.all_ansible_roles.empty?
-                     { action: (link_to _('Run all Ansible roles'), 'javascript:void(0);', disabled: true, title: 'No Roles assigned'), priority: 31 }
+                     { action: { content: (link_to _('Run all Ansible roles'), 'javascript:void(0);', disabled: true, title: 'No roles assigned', class: 'disabled'), options: { class: 'disabled' } }, priority: 31 }
                    else
                      { action: display_link_if_authorized(_('Run all Ansible roles'), hash_for_play_roles_hostgroup_path(id: hostgroup), 'data-no-turbolink': true, title: _('Run all Ansible roles on hosts belonging to this host group')), priority: 31 }
                    end
