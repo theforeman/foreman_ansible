00b6061bf5f2098b6317b73add3b67014170f4bb
Ron Lavi <1ronlavi@gmail.com>
2023-01-04 13:43:39 +0200

    Bump version 10.2.0
    after we modified REX version - to enable nightly tests to run

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '10.1.0'
+  VERSION = '10.2.0'
 end
