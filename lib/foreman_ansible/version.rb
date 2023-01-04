90c00b6a17587f9ebb672e92f9d13a6079f245d6
nofaralfasi <nalfassi@redhat.com>
2023-01-04 15:15:43 +0200

    Bump version 11.0.0
    Bump foreman_remote_execution to 9.0.0

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '10.2.0'
+  VERSION = '11.0.0'
 end
