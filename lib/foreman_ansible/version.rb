7eaff2b1e3066997a936e5dc7263e8852dbe699a
Ron Lavi <1ronlavi@gmail.com>
2023-02-09 15:52:14 +0200

    Bump version 10.4.0

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '10.3.0'
+  VERSION = '10.4.0'
 end
