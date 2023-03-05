722f7aac4bc46b4678bdc30926c2c093c46292ed
nofaralfasi <nalfassi@redhat.com>
2023-03-05 19:18:31 +0200

    Refs #36021 - disable ansible actions if hostgroup is empty
    Disable Ansible actions links for hostgroups with no roles or hosts

--- test/integration/hostgroup_js_test.rb
+++ test/integration/hostgroup_js_test.rb
@@ -10,21 +10,23 @@ class HostgroupJsTest < IntegrationTestWithJavascript
     FactoryBot.create(:host, :hostgroup_id => hostgroup_with_roles.id)
   end
 
-  test 'hostgroup without roles should have disabled link' do
+  test 'hostgroup without roles should have disabled links' do
     visit hostgroups_path(search: hostgroup.name)
 
     first_row = page.find('table > tbody > tr:nth-child(1)')
     first_row.find('td:nth-child(4) > div > a').click
 
     assert_includes first(:link, 'Run all Ansible roles')[:class], 'disabled'
+    assert_includes first(:link, 'Configure Ansible Job')[:class], 'disabled'
   end
 
-  test 'hostgroup with roles should have clickable link' do
+  test 'hostgroup with roles should have clickable links' do
     visit hostgroups_path(search: hostgroup_with_roles.name)
 
     first_row = page.find('table > tbody > tr:nth-child(1)')
     first_row.find('td:nth-child(4) > div > a').click
 
     assert_not first(:link, 'Run all Ansible roles')[:class].include?('disabled')
+    assert_not first(:link, 'Configure Ansible Job')[:class].include?('disabled')
   end
 end
