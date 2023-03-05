af7faf98f8d1d99e4108eb20f437e1c5e1106c3c
nofaralfasi <nalfassi@redhat.com>
2023-03-05 19:18:31 +0200

    Refs #36021 - disable ansible actions if hostgroup is empty
    Disable Ansible actions links for hostgroups with no roles or hosts

--- app/helpers/foreman_ansible/ansible_hostgroups_helper.rb
+++ app/helpers/foreman_ansible/ansible_hostgroups_helper.rb
@@ -4,18 +4,38 @@ module ForemanAnsible
   module AnsibleHostgroupsHelper
     def ansible_hostgroups_actions(hostgroup)
       actions = []
-      play_roles = if hostgroup.all_ansible_roles.empty?
-                     { action: { content: (link_to _('Run all Ansible roles'), 'javascript:void(0);', disabled: true, title: 'No roles assigned', class: 'disabled'), options: { class: 'disabled' } }, priority: 31 }
-                   else
-                     { action: display_link_if_authorized(_('Run all Ansible roles'), hash_for_play_roles_hostgroup_path(id: hostgroup), 'data-no-turbolink': true, title: _('Run all Ansible roles on hosts belonging to this host group')), priority: 31 }
-                   end
+      is_hostgroup_empty = hostgroup.all_ansible_roles.empty? || hostgroup.hosts_count.zero?
 
-      assign_jobs = { action: { content: (link_to _('Configure Ansible Job'), "/ansible/hostgroups/#{hostgroup.id}", class: 'la') }, priority: 32 }
+      if User.current.can?(:create_job_invocations)
+        actions << {
+          action: if is_hostgroup_empty
+                    disabled_action_link(_('Run all Ansible roles'))
+                  else
+                    display_link_if_authorized(_('Run all Ansible roles'), hash_for_play_roles_hostgroup_path(id: hostgroup), 'data-no-turbolink': true, title: _('Run all Ansible roles on hosts belonging to this host group'))
+                  end,
+          priority: 31
+        }
+      end
 
-      actions.push play_roles if User.current.can?(:create_job_invocations)
-      actions.push assign_jobs if User.current.can?(:view_job_invocations) && User.current.can?(:view_recurring_logics)
+      if User.current.can?(:view_job_invocations) && User.current.can?(:view_recurring_logics)
+        actions << {
+          action: if is_hostgroup_empty
+                    disabled_action_link(_('Configure Ansible Job'))
+                  else
+                    link_to(_('Configure Ansible Job'), "/ansible/hostgroups/#{hostgroup.id}", class: 'la')
+                  end,
+          priority: 32
+        }
+      end
 
       actions
+    end
+
+    def disabled_action_link(text)
+      {
+        content: link_to(text, 'javascript:void(0);', disabled: true, title: _('No roles/hosts assigned'), class: 'disabled'),
+        options: { class: 'disabled' }
+      }
     end
   end
 end
