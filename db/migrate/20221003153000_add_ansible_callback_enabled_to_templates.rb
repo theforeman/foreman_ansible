3a26b1c20e594bf642b5f8aa30deefd1951a7aae
nofaralfasi <nalfassi@redhat.com>
2023-04-24 19:55:55 +0300

    Fixes #36299 - Fix AddAnsibleCallbackEnabledToTemplates migration

--- db/migrate/20221003153000_add_ansible_callback_enabled_to_templates.rb
+++ db/migrate/20221003153000_add_ansible_callback_enabled_to_templates.rb
@@ -4,7 +4,7 @@ class AddAnsibleCallbackEnabledToTemplates < ActiveRecord::Migration[6.0]
   def change
     add_column :templates, :ansible_callback_enabled, :boolean, default: false
     RemoteExecutionFeature.where(label: 'ansible_run_host').each do |rex_feature|
-      Template.find(rex_feature.job_template_id).update(ansible_callback_enabled: true)
+      Template.where(id: rex_feature.job_template_id).update_all(ansible_callback_enabled: true)
     end
   end
 end
