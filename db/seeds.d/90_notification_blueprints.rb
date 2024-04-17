ebb158e857240b8e7d75a3f04b90401ac2e6a1a1
Leos Stejskal <lstejska@redhat.com>
2024-04-17 08:27:12 +0200

    SAT-22989 - Remove  'Run Insights maintenance plan'

--- db/seeds.d/90_notification_blueprints.rb
+++ db/seeds.d/90_notification_blueprints.rb
@@ -2,19 +2,6 @@
 
 blueprints = [
   {
-    :group => N_('Jobs'),
-    :name => 'insights_remediation_successful',
-    :message => N_('Insights remediation on %{hosts_count}' \
-                   ' host(s) has finished successfully'),
-    :level => 'success',
-    :actions => {
-      :links => [
-        :path_method => :job_invocation_path,
-        :title => N_('Job Details')
-      ]
-    }
-  },
-  {
     :group => N_('Roles'),
     :name => 'Sync_roles_and_variables_successfully',
     :message => N_('Import roles and variables has finished successfully'),
