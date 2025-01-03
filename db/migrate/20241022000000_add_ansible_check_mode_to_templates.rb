c482f89c611fb28637e4fd0186823371ef619b0b
Ben Erickson <bnerickson87@gmail.com>
2025-01-03 10:48:32 -0800

    Removing unnecessary frozen_string_literal statement, removing unnecessary template update section, updating rails migration version to 7.0

--- db/migrate/20241022000000_add_ansible_check_mode_to_templates.rb
+++ db/migrate/20241022000000_add_ansible_check_mode_to_templates.rb
@@ -1,10 +1,5 @@
-# frozen_string_literal: true
-
-class AddAnsibleCheckModeToTemplates < ActiveRecord::Migration[6.0]
+class AddAnsibleCheckModeToTemplates < ActiveRecord::Migration[7.0]
   def change
     add_column :templates, :ansible_check_mode, :boolean, default: false
-    RemoteExecutionFeature.where(label: 'ansible_run_host').each do |rex_feature|
-      Template.where(id: rex_feature.job_template_id).update_all(ansible_check_mode: false)
-    end
   end
 end
