c02973ae141ab9529a256a6289eb503cab3705a7
Adam Ruzicka <aruzicka@redhat.com>
2026-02-10 12:39:34 +0100

    i18n - pulling from tx

--- app/assets/javascripts/foreman_ansible/locale/ko/foreman_ansible.js
+++ app/assets/javascripts/foreman_ansible/locale/ko/foreman_ansible.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_ansible": {
       "": {
-        "Project-Id-Version": "foreman_ansible 17.0.1",
+        "Project-Id-Version": "foreman_ansible 17.0.2",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2018-11-14 21:36+0000",
         "Last-Translator": "Ewoud Kohl van Wijngaarden <ewoud+transifex@kohlvanwijngaarden.nl>, 2025",
