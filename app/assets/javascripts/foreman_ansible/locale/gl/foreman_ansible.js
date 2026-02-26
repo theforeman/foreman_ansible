0a553d7237539739f9bfc362a92a3fad219627ba
alazik <alazik@redhat.com>
2026-02-26 16:14:07 +0100

    i18n - pulling from tx

--- app/assets/javascripts/foreman_ansible/locale/gl/foreman_ansible.js
+++ app/assets/javascripts/foreman_ansible/locale/gl/foreman_ansible.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_ansible": {
       "": {
-        "Project-Id-Version": "foreman_ansible 17.0.2",
+        "Project-Id-Version": "foreman_ansible 17.0.3",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2018-11-14 21:36+0000",
         "Last-Translator": "Carmela Rubiños <carmela.rubinos@gmail.com>, 2023",
