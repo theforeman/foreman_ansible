e740193c0f09870bcfa4e55bcbea060393ffff9a
Adam Ruzicka <aruzicka@redhat.com>
2026-08-05 09:26:06 +0200

    Bump version to 18.0.1

--- lib/foreman_ansible/version.rb
+++ lib/foreman_ansible/version.rb
@@ -4,5 +4,5 @@
 # This way other parts of Foreman can just call ForemanAnsible::VERSION
 # and detect what version the plugin is running.
 module ForemanAnsible
-  VERSION = '18.0.0'
+  VERSION = '18.0.1'
 end
